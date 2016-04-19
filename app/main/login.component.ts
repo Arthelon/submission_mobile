b7b917e74ef51679e84e5b0200d15e485b734926
Arthelon <hsing.daniel@gmail.com>
2016-04-19 22:11:05 +0800

    Implemented registration form validation

--- app/main/login.component.ts
+++ app/main/login.component.ts
@@ -1,6 +1,7 @@
-import {Component} from 'angular2/core'
-import {FORM_DIRECTIVES, Validators, AbstractControl, ControlGroup, FormBuilder} from 'angular2/common'
+import {Component, provide, OpaqueToken} from 'angular2/core'
+import {FORM_DIRECTIVES, Validators, AbstractControl, ControlGroup, FormBuilder, Control} from 'angular2/common'
 import {NavController, IONIC_DIRECTIVES} from 'ionic-angular'
+import {Http} from 'angular2/http'
 
 @Component({
 	templateUrl: 'build/main/login.html',
@@ -12,12 +13,17 @@ export class LoginComponent {
 	username: AbstractControl
 	password: AbstractControl
 
-	constructor(fb: FormBuilder) {
+	constructor(private fb: FormBuilder, private http: Http) {
 		this.loginForm = fb.group({
 			'username': ['', Validators.required],
 			'password': ['', Validators.required]
 		})
 		this.username = this.loginForm.controls['username']
 		this.password = this.loginForm.controls['password']
+	}
+
+	onSubmit(val) {
+		console.log(val)
+		// this.http.post(this.CONFIG.SERVER_URL, val)
 	}
 }
--- app/main/register.component.ts
+++ app/main/register.component.ts
@@ -1,8 +1,54 @@
 import {Component} from 'angular2/core'
+import {FORM_DIRECTIVES, Validators, AbstractControl, ControlGroup, FormBuilder, Control} from 'angular2/common'
+import {NavController, IONIC_DIRECTIVES} from 'ionic-angular'
+import {ValidationService} from '../services/validators.ts'
 
 @Component({
-	template: ``
+	templateUrl: 'build/main/register.html',
+	directives: [IONIC_DIRECTIVES],
+	selector: 'register-comp'
 })
 export class RegisterComponent {
+	registerForm: ControlGroup
+	//Form fields
+	username: AbstractControl
+	password: AbstractControl
+	password2: AbstractControl
+	email: AbstractControl
+	first_name: AbstractControl
+	last_name: AbstractControl
 
+	constructor(fb: FormBuilder) {
+		//ControlGroup setup for form fields
+		this.registerForm = fb.group({
+			'username': ['', Validators.required],
+			'password': ['', Validators.compose([Validators.required, Validators.minLength(8)])],
+			'confirmPassword': ['', Validators.required],
+			'email': ['', Validators.compose([Validators.required, ValidationService.emailValidator])],
+			'first_name': ['', Validators.required],
+			'last_name': ['', Validators.required]
+		}, {validator: this.matchingPasswords('password', 'confirmPassword')})
+		//Fields initialized with Control instances
+		this.password = this.registerForm.controls['password']
+		this.password2 = this.registerForm.controls['confirmPassword']
+		this.first_name = this.registerForm.controls['first_name']
+		this.last_name = this.registerForm.controls['last_name']
+		this.email = this.registerForm.controls['email']
+		this.username = this.registerForm.controls['username']
+
+	}
+
+	//Password validation function
+	matchingPasswords(passwordKey: string, passwordConfirmationKey: string) {
+		return (group: ControlGroup) => {
+			let passwordInput = group.controls[passwordKey];
+			let passwordConfirmationInput = group.controls[passwordConfirmationKey];
+			if (passwordInput.value !== passwordConfirmationInput.value) {
+				return passwordConfirmationInput.setErrors({ notEquivalent: true })
+			}
+		}
+	}
+	onSubmit(val: string) {
+		console.log(val)
+	}
 }
